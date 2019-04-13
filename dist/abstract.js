6a3f2334bf468178111eab9acbe9bd3da1879f0c
Yariv <yluts@outbrain.com>
2019-04-13 20:24:58 +0300

    remove dist

--- dist/abstract.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ModelAbstract = /** @class */ (function () {
-    function ModelAbstract() {
-    }
-    return ModelAbstract;
-}());
-exports.ModelAbstract = ModelAbstract;
-//# sourceMappingURL=abstract.js.map
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './repository';
-export * from './query';
-export * from './interfaces/model.interface';
-export * from './decorators/model';
-export * from './decorators/field';
