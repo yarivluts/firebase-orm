49f2b844a509815116e1be9cccbf32a107645943
Yariv <yluts@outbrain.com>
2019-04-19 01:55:28 +0300

    move dependence to peer export interfaces

--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
 export * from './repository';
 export * from './query';
-export * from './interfaces/model.interface';
 export * from './decorators/model';
 export * from './decorators/field';
+
+
+//Interfces
+export * from './interfaces/model.interface';
+export * from './interfaces/current.model.interface';
+export * from './interfaces/field.options.interface';
+export * from './interfaces/model.alllist.options.interface';
+export * from './interfaces/model.options.interface';
+export * from './interfaces/observe.load.model.interface';
+export * from './interfaces/observe.remove.model.interface';
+export * from './interfaces/observe.save.model.interface';
